b09d5d2dbbba252ce12fc63a9a069533f41a55bd
Gerd Aschemann <gerd@aschemann.net>
2021-03-09 18:01:41 +0100

    Jenkins-X needs DNS to work properly

--- bin/install-all.sh
+++ bin/install-all.sh
@@ -15,7 +15,7 @@ $sudo snap install microk8s --classic
 $sudo usermod -a -G microk8s $USER
 test -d ~/.kube && $sudo chown -f -R $USER ~/.kube
 
-$sudo microk8s enable ingress helm3 storage
+$sudo microk8s enable ingress helm3 storage dns
 
 # $sudo echo 'k8s.galinews.intra.aschemann.net' >> /var/snap/
 
